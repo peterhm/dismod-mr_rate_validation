721b4a85bb9a01ecebb30dd59637c516ae8a1cce
Hannah Peterson <peterhm@uw.edu>
2013-01-29 13:31:03 -0800

    bug: std and area fixes

--- convert_data_in.py
+++ convert_data_in.py
@@ -37,20 +37,20 @@ def build_data_in(dm3, data_type, model_num):
     return data_in
 
 def data(dm3, data_type):
+    # find standard error and use it for standard deviation
+    dm3 = mu.create_uncertainty(dm3, 'log_normal')
     # create data file
     data_in = empty_data_in(dm3.input_data.index)
     data_in['integrand'] = convert_data_type(data_type)
     data_in['meas_value'] = dm3.input_data['value']
+    data_in['meas_stdev'] = dm3.input_data['standard_error']
     data_in['sex'] = dm3.input_data['sex']
     data_in['age_lower'] = dm3.input_data['age_start']
     data_in['age_upper'] = dm3.input_data['age_end']
     data_in['time_lower'] = dm3.input_data['year_start']
     data_in['time_upper'] =  dm3.input_data['year_end']
     data_in['x_sex'] = dm3.input_data['sex'].map(dict(male=.5, female=-.5, total=0))
-    data_in = data_in.append(model.input_data.filter(like='x_'))
-    # find standard error and use it for standard deviation
-    dm3 = mu.create_uncertainty(dm3, 'log_normal')
-    data_in['meas_stdev'] = dm3.input_data['standard_error']
+    data_in = data_in.append(dm3.input_data.filter(like='x_'))
     return data_in
 
 def data_area(dm3, data_type, model_num):
@@ -61,13 +61,14 @@ def data_area(dm3, data_type, model_num):
     country = set(pl.flatten([[model.hierarchy.neighbors(r) for r in model.hierarchy.neighbors(sr)] for sr in model.hierarchy.neighbors('all')]))
     # create data area levels
     data_in = empty_data_in(dm3.input_data.index)
-    for i in dm3.get_data(data_type).index:
+    for i in dm3.input_data.index:
         if dm3.input_data.ix[i,'area'] in country:
-            data_in['m_sub'] = dm3.input_data.ix[i,'area']
-            data_in['m_region'] = model.hierarchy.in_edges(dm3.input_data.ix[i,'area'])[0][0]
-            data_in['m_super'] = model.hierarchy.in_edges(model.hierarchy.in_edges(dm3.input_data.ix[i,'area'])[0][0])[0][0]
+            data_in.ix[i,'m_sub'] = dm3.input_data.ix[i,'area']
+            data_in.ix[i,'m_region'] = model.hierarchy.in_edges(dm3.input_data.ix[i,'area'])[0][0]
+            data_in.ix[i,'m_super'] = model.hierarchy.in_edges(model.hierarchy.in_edges(dm3.input_data.ix[i,'area'])[0][0])[0][0]
         elif dm3.input_data.ix[i,'area'] in region:
-            data_in['m_region'] = dm3.input_data.ix[i,'area']
-            data_in['m_super'] = model.hierarchy.in_edges(dm3.input_data.ix[i,'area'])
+            data_in.ix[i,'m_region'] = dm3.input_data.ix[i,'area']
+            data_in.ix[i,'m_super'] = model.hierarchy.in_edges(dm3.input_data.ix[i,'area'])[0][0]
         elif dm3.input_data.ix[i,'area'] in superregion:
-            data_in['m_super'] = dm3.input_data.ix[i,'area']
+            data_in.ix[i,'m_super'] = dm3.input_data.ix[i,'area']
+    return data_in
