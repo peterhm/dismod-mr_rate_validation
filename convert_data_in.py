cf9895fbae270a4f007c495f182ad0daa6a61a84
Hannah Peterson <peterhm@uw.edu>
2013-01-29 16:20:25 -0800

    bug: area data now merged, not appended

--- convert_data_in.py
+++ convert_data_in.py
@@ -31,12 +31,6 @@ def empty_data_in(ix):
     return pandas.DataFrame(index=ix, columns=['integrand', 'meas_value', 'meas_stdev', 'sex', 'age_lower', 'age_upper', 'time_lower', 'time_upper', 'm_sub', 'm_region', 'm_super', 'x_sex'], dtype=object)
     
 def build_data_in(dm3, data_type, model_num):
-    # create data_in csv with appropriate fields
-    data_in = data(dm3, data_type)
-    data_in = data_in.append(data_area(dm3, data_type, model_num), ignore_index=True)
-    return data_in
-
-def data(dm3, data_type):
     # find standard error and use it for standard deviation
     dm3 = mu.create_uncertainty(dm3, 'log_normal')
     # create data file
@@ -46,21 +40,17 @@ def data(dm3, data_type):
     data_in['meas_stdev'] = dm3.input_data['standard_error']
     data_in['sex'] = dm3.input_data['sex']
     data_in['age_lower'] = dm3.input_data['age_start']
-    data_in['age_upper'] = dm3.input_data['age_end']
+    data_in['age_upper'] = dm3.input_data['age_end'] + 1.0
     data_in['time_lower'] = dm3.input_data['year_start']
-    data_in['time_upper'] =  dm3.input_data['year_end']
+    data_in['time_upper'] =  dm3.input_data['year_end'] + 1.0
     data_in['x_sex'] = dm3.input_data['sex'].map(dict(male=.5, female=-.5, total=0))
-    data_in = data_in.append(dm3.input_data.filter(like='x_'))
-    return data_in
-
-def data_area(dm3, data_type, model_num):
+    #data_in = data_in.append(dm3.input_data.filter(like='x_'))
     # create data hierarchy
     model = mu.load_new_model(model_num, 'all', data_type)
     superregion = set(model.hierarchy.neighbors('all'))
     region = set(pl.flatten([model.hierarchy.neighbors(sr) for sr in model.hierarchy.neighbors('all')]))
     country = set(pl.flatten([[model.hierarchy.neighbors(r) for r in model.hierarchy.neighbors(sr)] for sr in model.hierarchy.neighbors('all')]))
     # create data area levels
-    data_in = empty_data_in(dm3.input_data.index)
     for i in dm3.input_data.index:
         if dm3.input_data.ix[i,'area'] in country:
             data_in.ix[i,'m_sub'] = dm3.input_data.ix[i,'area']
