012f329bdf7accf7d752548bc52dbd07b3902833
Hannah Peterson <peterhm@uw.edu>
2013-02-01 15:43:57 -0800

    enh: update path for saving

--- model_conversion.py
+++ model_conversion.py
@@ -39,7 +39,7 @@ def build_param_in(dm3, thin, iter):
         param_in.ix[8] = 'tau_one_cov',
     return param_in
 
-def dm3rep_initialize(model_num, data_type, area, default=False):
+def dm3rep_initialize(model_num, data_type, area, thin, iter, path, bare_bones=False):
     '''
     Parameters
     ----------
@@ -50,29 +50,35 @@ def dm3rep_initialize(model_num, data_type, area, default=False):
       'p', 'i', 'r', 'f', 'pf', 'csmr', 'rr', 'smr', 'X'
     area : str
       level of heirarchy to keep
-    default : bool
+    thin : int
+      thinning number for MCMC
+    iter : int
+      number of iterations in MCMC
+    path : str
+      path to save parameter files in
+    bare_bones : bool
       True creates minimalist files, False uses DisMod-MR values, default set to False
     Results
     -------
     gets data and builds necessary files
-    .. Note :: If default is False, parameter files must be filled.
+    .. Note :: If bare_bones is False, parameter files must be filled.
     '''
     cwd = os.getcwd()
     os.chdir('/homes/peterhm/dismod_spline-20130115/build')
     # creates data
     os.system('bin/get_data.py %s' %model_num)
     # creates necessary files
-    if default == True: os.system('bin/fit.sh %s %s' %(model_num, c_data.convert_data_type(data_type))) # creates brad's default files
+    if bare_bones == True: os.system('bin/fit.sh %s %s' %(model_num, c_data.convert_data_type(data_type))) # creates brad's default files
     else:
         # load data structure
         dm3 = mu.load_new_model(model_num, area, data_type)
         # create required files
         prior_in = c_prior.build_prior_in(dm3, data_type, model_num)
-        prior_in.to_csv('/clustertmp/dismod/%s_prior_in.csv'%(model_num),index=False)
+        prior_in.to_csv(path + '%s_prior_in.csv'%(model_num),index=False)
         parameter_in = build_param_in(dm3, thin, iter)
-        parameter_in.to_csv('/clustertmp/dismod/%s_parameter_in.csv'%(model_num),index=False)
+        parameter_in.to_csv(path + %s_parameter_in.csv'%(model_num),index=False)
         data_in = c_data.build_data_in(dm3, data_type, model_num)
-        data_in.to_csv('/clustertmp/dismod/%s_prior_in.csv'%(model_num),index=False)
+        data_in.to_csv(path + '%s_prior_in.csv'%(model_num),index=False)
     # return to working directory
     os.chdir('%s' %cwd)
     # return dataframes to add
