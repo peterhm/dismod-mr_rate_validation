626bece89bddd678e82a777f07a7ed034df05322
Hannah Peterson <peterhm@uw.edu>
2013-02-01 15:00:48 -0800

    enh: parameter_in.csv function

--- model_conversion.py
+++ model_conversion.py
@@ -15,7 +15,29 @@ reload(c_prior)
 import convert_data_in as c_data
 reload(c_data)
 
-
+def build_param_in(dm3, thin, iter):
+    param_in = pandas.DataFrame(index=range(9), columns=['name','value'], dtype=object)
+    param_in.ix[0] = 'data_model','gaussian'
+    param_in.ix[1] = 'parameter_model','gaussian'
+    param_in.ix[2] = 'sample_interval', thin
+    param_in.ix[3] = 'num_sample', thin*iter
+    param_in.ix[4] = 'watch_interval',50
+    param_in.ix[5] = 'random_seed',0
+    # find eta and zeta values from data
+    nonzeros = (dm3.input_data['value'] != 0)
+    min_nonzero = min(dm3.input_data['value'][nonzeros])
+    if min_nonzero < 1.:
+        val = (min_nonzero)**2
+    elif dm3.input_data['value'].mean() >= 1.:
+        val = 1e-4 
+    param_in.ix[6] = 'zeta',val
+    param_in.ix[7] = 'eta',val
+    # find z_ covariates
+    if len(dm3.input_data.filter(like='z_').columns) == 1:
+        param_in.ix[8] = 'tau_one_cov',dm3.input_data.filter(like='z_').columns[0]
+    else:
+        param_in.ix[8] = 'tau_one_cov',
+    return param_in
 
 def dm3rep_initialize(model_num, data_type, area, default=False):
     '''
@@ -46,14 +68,13 @@ def dm3rep_initialize(model_num, data_type, area, default=False):
         dm3 = mu.load_new_model(model_num, area, data_type)
         # create required files
         prior_in = c_prior.build_prior_in(dm3, data_type, model_num)
-        #prior_in.to_csv()
-        parameter_in = pandas.DataFrame(columns=['name','value'])
+        prior_in.to_csv('/clustertmp/dismod/%s_prior_in.csv'%(model_num),index=False)
+        parameter_in = build_param_in(dm3, thin, iter)
+        parameter_in.to_csv('/clustertmp/dismod/%s_parameter_in.csv'%(model_num),index=False)
         data_in = c_data.build_data_in(dm3, data_type, model_num)
-        data_in.to_csv('/homes/peterhm/dismod_spline-20130115/data/dm-%s'%(model_num))
-        #sample_out.csv = 
-        info_out = pandas.DataFrame(columns=['name','value'])
+        data_in.to_csv('/clustertmp/dismod/%s_prior_in.csv'%(model_num),index=False)
     # return to working directory
     os.chdir('%s' %cwd)
     # return dataframes to add
-    return prior_in, parameter_in, info_out
+    
 
