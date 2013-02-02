4d065348ed1fd1e57f69c5dc0e4ec23bd9aaaeb8
Hannah Peterson <peterhm@uw.edu>
2013-02-01 16:50:17 -0800

    bug: old pandas treats inf as null

--- convert_prior_in.py
+++ convert_prior_in.py
@@ -36,9 +36,9 @@ def prior_noise(dm3, data_type):
     prior_in = empty_prior_in(range(6))
     prior_in['type'] = 'noise'
     # create xi from smoothness (values from ism.py)
-    smoothing_dict = {'No Prior':pl.inf, 'Slightly':.5, 'Moderately': .05, 'Very': .005}
+    smoothing_dict = {'No Prior':'inf', 'Slightly':.5, 'Moderately': .05, 'Very': .005}
     prior_in.ix[0,'name'] = 'xi'
-    prior_in.ix[0,'std'] = pl.inf
+    prior_in.ix[0,'std'] = 'inf'
     try:
         prior_in.ix[0,'mean'] = smoothing_dict[dm3.parameters[data_type]['smoothness']['amount']]
         prior_in.ix[0,'lower'] = smoothing_dict[dm3.parameters[data_type]['smoothness']['amount']]
@@ -48,18 +48,18 @@ def prior_noise(dm3, data_type):
         prior_in.ix[0,'lower'] = smoothing_dict['No Prior']
         prior_in.ix[0,'upper'] = smoothing_dict['No Prior']
     # create tau_zero from data heterogeneity
-    hetero_dict = {'No Prior':pl.inf, 'Slightly':.05, 'Moderately': .05, 'Very': .05}
+    hetero_dict = {'No Prior':'inf', 'Slightly':.05, 'Moderately': .05, 'Very': .05}
     prior_in.ix[1,'name'] = 'tau_zero'
     prior_in.ix[1,'mean'] = hetero_dict[dm3.parameters[data_type]['heterogeneity']]
-    prior_in.ix[1,'std'] = pl.inf
+    prior_in.ix[1,'std'] = 'inf'
     prior_in.ix[1,'lower'] = hetero_dict[dm3.parameters[data_type]['heterogeneity']]
     prior_in.ix[1,'upper'] = hetero_dict[dm3.parameters[data_type]['heterogeneity']]
     # create tau_one
     prior_in.ix[2,'name'] = 'tau_one'
     prior_in.ix[2,'mean'] = 0.
     prior_in.ix[2,'std'] = .25
-    prior_in.ix[2,'lower'] = -pl.inf
-    prior_in.ix[2,'upper'] = pl.inf
+    prior_in.ix[2,'lower'] = '-inf'
+    prior_in.ix[2,'upper'] = 'inf'
     # create gamma_* priors
     prior_in.ix[3,'name'] = 'gamma_sub'
     prior_in.ix[4,'name'] = 'gamma_region'
@@ -78,7 +78,7 @@ def prior_level(dm3, data_type):
     prior_in['name'] = dm3.parameters[data_type]['parameter_age_mesh']
     prior_in['lower'] = dm3.parameters[data_type]['level_bounds']['lower']
     prior_in['upper'] = dm3.parameters[data_type]['level_bounds']['upper']
-    prior_in['std'] = pl.inf
+    prior_in['std'] = 'inf'
     # fill age-dependent variables (mean)
     for i,a in enumerate(dm3.parameters[data_type]['parameter_age_mesh']):
         if dm3.parameters[data_type]['level_value']['age_before'] < dm3.parameters[data_type]['level_value']['age_after']:
@@ -98,21 +98,21 @@ def prior_direction(dm3, data_type):
     # fill non-age-dependent variables
     prior_in['type'] = 'dknot'
     prior_in['name'] = dm3.parameters[data_type]['parameter_age_mesh'][:-1]
-    prior_in['std'] = pl.inf
+    prior_in['std'] = 'inf'
     if dm3.parameters[data_type]['increasing']['age_start'] !=  dm3.parameters[data_type]['increasing']['age_end']:
         for i,a in enumerate(dm3.parameters[data_type]['parameter_age_mesh'][:-1]):
             if (dm3.parameters[data_type]['increasing']['age_start'] <= a < dm3.parameters[data_type]['increasing']['age_end']):
                 prior_in.ix[i,'lower'] = 0.
-                prior_in.ix[i,'upper'] = pl.inf
+                prior_in.ix[i,'upper'] = 'inf'
                 prior_in.ix[i,'mean'] = 1.
     if dm3.parameters[data_type]['decreasing']['age_start'] !=  dm3.parameters[data_type]['decreasing']['age_end']:
         for i,a in enumerate(dm3.parameters[data_type]['parameter_age_mesh'][:-1]):
             if (dm3.parameters[data_type]['decreasing']['age_start'] <= a < dm3.parameters[data_type]['decreasing']['age_end']):
-                prior_in.ix[i,'lower'] = - pl.inf
+                prior_in.ix[i,'lower'] = - 'inf'
                 prior_in.ix[i,'upper'] = 0.
                 prior_in.ix[i,'mean'] = -1.
-    prior_in['lower'] = prior_in['lower'].fillna(-pl.inf)
-    prior_in['upper'] = prior_in['upper'].fillna(pl.inf)
+    prior_in['lower'] = prior_in['lower'].fillna('-inf')
+    prior_in['upper'] = prior_in['upper'].fillna('inf')
     prior_in['mean'] = prior_in['mean'].fillna(0.)
     return prior_in    
     
@@ -122,8 +122,8 @@ def prior_m_area(dm3, model_num, data_type):
     prior_in['name'] = pl.unique(dm3.input_data['area'])
     prior_in['mean'] = 0.
     prior_in['std'] = 1.
-    prior_in['lower'] = -pl.inf
-    prior_in['upper'] = pl.inf
+    prior_in['lower'] = '-inf'
+    prior_in['upper'] = 'inf'
     # create hierarchy
     model = mu.load_new_model(model_num, 'all', data_type)
     superregion = set(model.hierarchy.neighbors('all'))
@@ -150,13 +150,13 @@ def prior_cov(dm3, data_type):
         try:
             prior_in.ix[i,'mean'] = dm3.parameters[data_type]['fixed_effects'][c]['mu']
             prior_in.ix[i,'std'] = dm3.parameters[data_type]['fixed_effects'][c]['sigma']
-            prior_in['lower'] = -pl.inf
-            prior_in['upper'] = pl.inf
+            prior_in['lower'] = '-inf'
+            prior_in['upper'] = 'inf'
         except KeyError:
             prior_in['mean'] = 0.
             prior_in['std'] = 1.
-            prior_in['lower'] = -pl.inf
-            prior_in['upper'] = pl.inf
+            prior_in['lower'] = '-inf'
+            prior_in['upper'] = 'inf'
     return prior_in
 
 
