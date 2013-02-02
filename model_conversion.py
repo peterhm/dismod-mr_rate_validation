c35b6d25a13a7baa5a586cada73158dc1455e7fc
Hannah Peterson <peterhm@uw.edu>
2013-02-01 16:06:03 -0800

    enh: refactor/clean up

--- model_conversion.py
+++ model_conversion.py
@@ -15,14 +15,14 @@ reload(c_prior)
 import convert_data_in as c_data
 reload(c_data)
 
-def build_param_in(dm3, thin, iter):
+def build_param_in(dm3, thin, iter, replicate):
     param_in = pandas.DataFrame(index=range(9), columns=['name','value'], dtype=object)
     param_in.ix[0] = 'data_model','gaussian'
     param_in.ix[1] = 'parameter_model','gaussian'
     param_in.ix[2] = 'sample_interval', thin
     param_in.ix[3] = 'num_sample', thin*iter
     param_in.ix[4] = 'watch_interval',50
-    param_in.ix[5] = 'random_seed',0
+    param_in.ix[5] = 'random_seed',replicate
     # find eta and zeta values from data
     nonzeros = (dm3.input_data['value'] != 0)
     min_nonzero = min(dm3.input_data['value'][nonzeros])
@@ -39,7 +39,7 @@ def build_param_in(dm3, thin, iter):
         param_in.ix[8] = 'tau_one_cov',
     return param_in
 
-def dm3rep_initialize(model_num, data_type, area, thin, iter, path, bare_bones=False):
+def dm3rep_initialize(model_num, data_type, area, thin, iter, replicate, path, bare_bones=False):
     '''
     Parameters
     ----------
@@ -54,6 +54,8 @@ def dm3rep_initialize(model_num, data_type, area, thin, iter, path, bare_bones=F
       thinning number for MCMC
     iter : int
       number of iterations in MCMC
+    replicate : int
+      number for random number
     path : str
       path to save parameter files in
     bare_bones : bool
@@ -65,18 +67,18 @@ def dm3rep_initialize(model_num, data_type, area, thin, iter, path, bare_bones=F
     '''
     cwd = os.getcwd()
     os.chdir('/homes/peterhm/dismod_spline-20130115/build')
-    # creates data
-    os.system('bin/get_data.py %s' %model_num)
     # creates necessary files
-    if bare_bones == True: os.system('bin/fit.sh %s %s' %(model_num, c_data.convert_data_type(data_type))) # creates brad's default files
+    if bare_bones == True: 
+        # creates data
+        os.system('bin/get_data.py %s' %model_num)
     else:
         # load data structure
         dm3 = mu.load_new_model(model_num, area, data_type)
         # create required files
         prior_in = c_prior.build_prior_in(dm3, data_type, model_num)
         prior_in.to_csv(path + '%s_prior_in.csv'%(model_num),index=False)
-        parameter_in = build_param_in(dm3, thin, iter)
-        parameter_in.to_csv(path + %s_parameter_in.csv'%(model_num),index=False)
+        parameter_in = build_param_in(dm3, thin, iter, replicate)
+        parameter_in.to_csv(path + '%s_parameter_in.csv'%(model_num),index=False)
         data_in = c_data.build_data_in(dm3, data_type, model_num)
         data_in.to_csv(path + '%s_prior_in.csv'%(model_num),index=False)
     # return to working directory
